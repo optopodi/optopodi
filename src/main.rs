e8b9892527b4b9e9fbdfb98e086186f0c0252020
Niko Matsakis <niko@alum.mit.edu>
2021-06-05 05:56:37 -0400

    cleanup the code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::future::Future;
-
 use anyhow::Error;
 use fehler::throws;
-use octocrab::Page;
+use octocrab::models::Repository;
 mod token;
+mod util;
 
 #[throws]
 #[tokio::main]
@@ -14,7 +13,7 @@ async fn main() {
         .build()?;
 
     let rust_lang_org = octocrab.orgs("rust-lang");
-    let repos = all_repos(&&rust_lang_org).await?;
+    let repos: Vec<Repository> = all_repos(&&rust_lang_org).await?;
 
     for repo in &repos {
         println!("repo: {}", repo.name);
@@ -23,22 +22,5 @@ async fn main() {
 
 #[throws]
 async fn all_repos(org: &octocrab::orgs::OrgHandler<'_>) -> Vec<octocrab::models::Repository> {
-    accumulate_pages(|page| org.list_repos().page(page).send()).await?
-}
-
-#[throws]
-async fn accumulate_pages<T, F>(mut data: impl FnMut(u32) -> F) -> Vec<T>
-where
-    F: Future<Output = Result<Page<T>, octocrab::Error>>,
-{
-    let mut repos = vec![];
-    for page in 0_u32.. {
-        let page = data(page).await?;
-        let items_len = page.items.len();
-        repos.extend(page.items);
-        if items_len == 0 {
-            break;
-        }
-    }
-    return repos;
+    util::accumulate_pages(|page| org.list_repos().page(page).send()).await?
 }
