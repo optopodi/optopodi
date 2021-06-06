943f30130c321982ea79b787902453873c7da21a
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-06-05 20:02:05 -0500

    refactor 'count_valid_prs' to return type 'Option<Url>'

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use anyhow::Error;
 use chrono::{Duration, Utc};
 use fehler::throws;
-use octocrab::models::pulls::PullRequest;
-use octocrab::models::Repository;
+use octocrab::models::{pulls::PullRequest, Repository};
+use std::option::Option;
+use url::Url;
 mod token;
 mod util;
 
@@ -29,6 +30,28 @@ async fn all_repos(org: &octocrab::orgs::OrgHandler<'_>) -> Vec<octocrab::models
     util::accumulate_pages(|page| org.list_repos().page(page).send()).await?
 }
 
+/// count the number of pull requests created in the last 30 days for the given repository within the [`rust-lang` github organization](https://github.com/rust-lang)
+///
+/// ## Arguments
+///
+/// - `octo` — the instance of `octocrab::Octocrab` that should be used to make any GitHub queries
+/// - `repo_name` — The name of the repository (within GitHub Organization "rust-lang") to count pull-requests for
+///
+/// ## Example
+///
+/// ```
+/// use github-metrics;
+/// use octocrab;
+/// use std::string::String;
+///
+/// let octocrab_instance = octocrab::Octocrab::builder().personal_token("SOME_GITHUB_TOKEN").build()?;
+///
+/// const num_pull_requests = github-metrics::count_pull_requests(octocrab_instance, String::from("rust"));
+///
+/// println!("'rust-lang/rust' has had {} Pull Requests in the last 30 days!", num_pull_requests);
+///
+/// ```
+///
 #[throws]
 async fn count_pull_requests(octo: &octocrab::Octocrab, repo_name: &String) -> usize {
     let init_page = octo
@@ -40,10 +63,12 @@ async fn count_pull_requests(octo: &octocrab::Octocrab, repo_name: &String) -> u
         .await?;
 
     let thirty_days_ago = Utc::now() - Duration::days(30);
-    let mut pr_count: usize = init_page.items.len();
+    let mut pr_count: usize = 0;
     let mut next_page = init_page.next.to_owned();
 
-    let mut count_valid_prs = |page: octocrab::Page<PullRequest>| -> Option<()> {
+    // given a page of PRs, count any valid PRs and stop iteration as soon as we surpass our date range of 30 days
+    let mut count_valid_prs = |page: octocrab::Page<PullRequest>| -> Option<Url> {
+        let copy_next = page.next.to_owned();
         let mut all_valid = true;
         for pr in page {
             if pr.created_at < thirty_days_ago {
@@ -54,15 +79,16 @@ async fn count_pull_requests(octo: &octocrab::Octocrab, repo_name: &String) -> u
             }
         }
 
-        return if all_valid { Some(()) } else { None };
+        return if all_valid { copy_next } else { None };
     };
 
+    // run initial page of data through the counter; this might be the only page we have!
+    // note: this is set up this way b/c the `octo.get_page` different is called for all
+    // subsequent pages (rather than `octo.pulls()` which allows us all sorts of good specifications)
+    count_valid_prs(init_page);
+
     while let Some(page) = octo.get_page::<PullRequest>(&next_page).await? {
-        let copy_next = page.next.to_owned();
-        next_page = match count_valid_prs(page) {
-            Some(_) => copy_next,
-            _ => None,
-        }
+        next_page = count_valid_prs(page);
     }
 
     pr_count
