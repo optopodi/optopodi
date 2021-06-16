931846b23ad52866f57a059201404a3e48fe69cc
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-06-16 17:45:09 -0500

    add broken version of 'spawn_producer_task' for sake of conversation

--- src/main.rs
+++ src/main.rs
@@ -82,7 +82,7 @@ async fn main() {
             let column_names = list_repos.column_names();
 
             tokio::spawn(async move {
-                list_repos.spawn_producer_task(tx);
+                list_repos.producer_task(tx).await;
             });
 
             match gs {
