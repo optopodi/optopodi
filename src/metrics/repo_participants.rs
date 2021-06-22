17f8fcb42ba32703b896d064ba0442dee4167d8e
Niko Matsakis <niko@alum.mit.edu>
2021-06-22 11:19:55 -0400

    managed authored/reviewed

--- src/metrics/repo_participants.rs
+++ src/metrics/repo_participants.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use anyhow::Error;
 use async_trait::async_trait;
@@ -37,8 +37,7 @@ impl Producer for RepoParticipants {
             &self.repo_name,
             Duration::days(self.number_of_days),
         )
-        .await
-        .unwrap();
+        .await?;
 
         for participant in data {
             tx.send(vec![participant.login, participant.prs.to_string()])
@@ -90,7 +89,14 @@ async fn pr_participants(
         date_str = date_str,
     );
 
-    let mut data = HashMap::new();
+    // Tracks, for each github login, how many PRs they participated in on this repository.
+    let mut participated = HashMap::new();
+
+    // Tracks, for each github login, how many PRs they authored on this repository.
+    let mut authored: HashMap<String, u64> = HashMap::new();
+
+    // Tracks, for each github login, how many PRs they reviewed or merged on this repository.
+    let mut reviewed_or_merged: HashMap<String, u64> = HashMap::new();
 
     let response = PrsAndParticipants::execute(pap::Variables {
         query_string,
@@ -104,7 +110,15 @@ async fn pr_participants(
             Some(pap::PrsAndParticipantsSearchEdgesNode::PullRequest(pr)) => pr,
             _ => continue,
         };
-        let number = pr.number;
+
+        eprintln!("{:#?}", pr);
+
+        // For each person who participated on this PR, increment their
+        // entry in the `participated` map.
+        //
+        // Assumption: a given individual will not appear more than once
+        // in this list.
+        let mut participants_found = 0;
         for participant in pr
             .participants
             .edges
@@ -113,18 +127,65 @@ async fn pr_participants(
             .flatten()
             .map(|p| p.node)
             .flatten()
+            .inspect(|_| participants_found += 1)
         {
             let login = participant.login;
-            data.entry(login).or_insert(Vec::new()).push(number);
+            *participated.entry(login).or_insert(0) += 1;
+        }
+
+        // FIXME: We should eventually support the case that there are more than
+        // 100 participants, but for now, just assert that we don't need to deal
+        // with pagination. The way I would expect to handle this is to have a separate
+        // query in which we identify a PR by its internal ID and walk our way through
+        // the list of participants.
+        if participants_found != pr.participants.total_count {
+            anyhow::bail!("FIXME: pagination support for participants list");
+        }
+
+        // Count the number of PRs on which a person has issued a review.
+        let reviews = pr.reviews.unwrap();
+        let mut reviews_found = 0;
+
+        let reviewers: HashSet<String> = reviews
+            .nodes
+            .into_iter()
+            .flatten()
+            .inspect(|_| reviews_found += 1)
+            .flatten()
+            .flat_map(|n| n.author)
+            .flat_map(|a| match a.on {
+                pap::PrsAndParticipantsSearchEdgesNodeOnPullRequestReviewsNodesAuthorOn::User(
+                    u,
+                ) => Some(u.login),
+                _ => None,
+            })
+            .collect();
+        for reviewer in reviewers {
+            *reviewed_or_merged.entry(reviewer).or_insert(0) += 1;
+        }
+
+        if reviews_found != reviews.total_count {
+            anyhow::bail!("FIXME: pagination support for participants list");
+        }
+
+        // Count the number of PRs which a person has authored.
+        if let Some(a) = pr.author {
+            if let pap::PrsAndParticipantsSearchEdgesNodeOnPullRequestAuthorOn::User(u) = a.on {
+                *authored.entry(u.login).or_insert(0) += 1;
+            }
+        }
+
+        // Count the number of PRs which a person has merged.
+        if let Some(a) = pr.merged_by {
+            if let pap::PrsAndParticipantsSearchEdgesNodeOnPullRequestMergedByOn::User(u) = a.on {
+                *reviewed_or_merged.entry(u.login).or_insert(0) += 1;
+            }
         }
     }
 
     let mut counts = vec![];
-    for (login, pr_numbers) in data {
-        counts.push(Participant {
-            login,
-            prs: pr_numbers.len() as u64,
-        });
+    for (login, prs) in participated {
+        counts.push(Participant { login, prs });
     }
 
     counts.sort_by_key(|p| u64::MAX - p.prs);
